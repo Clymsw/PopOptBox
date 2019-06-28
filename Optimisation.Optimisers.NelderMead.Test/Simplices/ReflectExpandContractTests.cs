9902ebe92e462097fde1a6c8640f130946d700cb
Clym Stock-Williams <clymsw@gmail.com>
2019-06-28 22:10:41 +0200

    Added tests for Simplex operators

--- Optimisation.Optimisers.NelderMead.Test/Simplices/ReflectExpandContractTests.cs
+++ Optimisation.Optimisers.NelderMead.Test/Simplices/ReflectExpandContractTests.cs
@@ -1,4 +1,6 @@
+using Optimisation.Base.Variables;
 using System;
+using System.Linq;
 using Xunit;
 
 namespace Optimisation.Optimisers.NelderMead.Simplices.Test
@@ -17,6 +19,61 @@ namespace Optimisation.Optimisers.NelderMead.Simplices.Test
             else
                 new ReflectExpandContract(coefficient);
         }
-        
+
+        [Theory]
+        [InlineData(new[] { 1.0, 1.0 }, new[] { 0.0, 1 }, new[] { 1.0, 0 }, new[] { 0.0, 0 })]
+        public void Reflect_CalculatesCorrectNewLocation(double[] expectedAnswer, params double[][] testValues)
+        {
+            var theOperator = new ReflectExpandContract(1);
+
+            var ds = DecisionSpace.CreateForUniformDoubleArray(expectedAnswer.Length, double.MinValue, double.MaxValue);
+            var dvs = testValues.Select(v => DecisionVector.CreateFromArray(ds, v)).ToList();
+
+            var newLocation = theOperator.Operate(dvs);
+
+            Assert.Equal(expectedAnswer, newLocation.Vector.Select(d => (double)d));
+        }
+
+        [Theory]
+        [InlineData(new[] { 1.5, 1.5 }, new[] { 0.0, 1 }, new[] { 1.0, 0 }, new[] { 0.0, 0 })]
+        public void Expand_CalculatesCorrectNewLocation(double[] expectedAnswer, params double[][] testValues)
+        {
+            var theOperator = new ReflectExpandContract(2);
+
+            var ds = DecisionSpace.CreateForUniformDoubleArray(expectedAnswer.Length, double.MinValue, double.MaxValue);
+            var dvs = testValues.Select(v => DecisionVector.CreateFromArray(ds, v)).ToList();
+
+            var newLocation = theOperator.Operate(dvs);
+
+            Assert.Equal(expectedAnswer, newLocation.Vector.Select(d => (double)d));
+        }
+
+        [Theory]
+        [InlineData(new[] { 0.75, 0.75 }, new[] { 0.0, 1 }, new[] { 1.0, 0 }, new[] { 0.0, 0 })]
+        public void ContractOut_CalculatesCorrectNewLocation(double[] expectedAnswer, params double[][] testValues)
+        {
+            var theOperator = new ReflectExpandContract(0.5);
+
+            var ds = DecisionSpace.CreateForUniformDoubleArray(expectedAnswer.Length, double.MinValue, double.MaxValue);
+            var dvs = testValues.Select(v => DecisionVector.CreateFromArray(ds, v)).ToList();
+
+            var newLocation = theOperator.Operate(dvs);
+
+            Assert.Equal(expectedAnswer, newLocation.Vector.Select(d => (double)d));
+        }
+
+        [Theory]
+        [InlineData(new[] { 0.25, 0.25 }, new[] { 0.0, 1 }, new[] { 1.0, 0 }, new[] { 0.0, 0 })]
+        public void ContractIn_CalculatesCorrectNewLocation(double[] expectedAnswer, params double[][] testValues)
+        {
+            var theOperator = new ReflectExpandContract(-0.5);
+
+            var ds = DecisionSpace.CreateForUniformDoubleArray(expectedAnswer.Length, double.MinValue, double.MaxValue);
+            var dvs = testValues.Select(v => DecisionVector.CreateFromArray(ds, v)).ToList();
+
+            var newLocation = theOperator.Operate(dvs);
+
+            Assert.Equal(expectedAnswer, newLocation.Vector.Select(d => (double)d));
+        }
     }
 }
--- Optimisation.Optimisers.NelderMead.Test/Simplices/ShrinkTests.cs
+++ Optimisation.Optimisers.NelderMead.Test/Simplices/ShrinkTests.cs
@@ -1,4 +1,6 @@
+using Optimisation.Base.Variables;
 using System;
+using System.Linq;
 using Xunit;
 
 namespace Optimisation.Optimisers.NelderMead.Simplices.Test
@@ -17,6 +19,19 @@ namespace Optimisation.Optimisers.NelderMead.Simplices.Test
             else
                 new Shrink(coefficient);
         }
-        
+
+        [Theory]
+        [InlineData(new[] { 0, 0.5 }, new[] { 0.0, 1 }, new[] { 1.0, 0 }, new[] { 0.0, 0 })]
+        public void Shrink_CalculatesCorrectNewLocation(double[] expectedAnswer, params double[][] testValues)
+        {
+            var theOperator = new Shrink(0.5);
+
+            var ds = DecisionSpace.CreateForUniformDoubleArray(expectedAnswer.Length, double.MinValue, double.MaxValue);
+            var dvs = testValues.Select(v => DecisionVector.CreateFromArray(ds, v)).ToList();
+
+            var newLocation = theOperator.Operate(dvs);
+
+            Assert.Equal(expectedAnswer, newLocation.Vector.Select(d => (double)d));
+        }
     }
 }
