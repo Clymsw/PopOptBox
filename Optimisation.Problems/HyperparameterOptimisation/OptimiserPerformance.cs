2c2d811347e17177154604526879e9ac3840e3e0
Clym Stock-Williams <Clym.StockWilliams@InHolland.nl>
2019-07-12 17:03:46 +0200

    Optimisation.Problems.HyperparameterOptimisation created

---
+++ Optimisation.Problems/HyperparameterOptimisation/OptimiserPerformance.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Optimisation.Base.Variables;
+
+namespace Optimisation.Problems.HyperparameterOptimisation
+{
+    /// <summary>
+    /// Captures information on the performance of an optimisation algorithm on a given test problem.
+    /// Immutable by design.
+    /// </summary>
+    public sealed class OptimiserPerformance
+    {
+        // Definitions
+        private readonly string optimiser; 
+        private readonly DecisionVector hyperParameters;
+        private readonly string problem;
+        
+        // Results
+        private readonly List<double> fitnessResults;
+        private readonly List<TimeSpan> timesToConverge;
+        private readonly List<int> evaluationsToConverge;
+        private readonly List<int> evaluationsToFindBest;
+
+        #region Constructor
+
+        public OptimiserPerformance(string optimiser, DecisionVector hyperParameters, string problem,
+            List<double> fitnessResults, List<TimeSpan> timesToConverge, 
+            List<int> evaluationsToConverge,List<int> evaluationsToFindBest)
+        {
+            this.optimiser = optimiser;
+            this.hyperParameters = hyperParameters;
+            this.problem = problem;
+            this.fitnessResults = fitnessResults;
+            this.evaluationsToConverge = evaluationsToConverge;
+            this.timesToConverge = timesToConverge;
+            this.evaluationsToFindBest = evaluationsToFindBest;
+        }
+
+        #endregion 
+        
+        public double MeanFitness => fitnessResults.Average();
+
+        public double BestFitness => fitnessResults.Min();
+        
+        public double ProportionOfFitnessValuesWithin(double tolerance)
+        {
+            if (tolerance <= 0)
+                throw new ArgumentOutOfRangeException(nameof(tolerance), "Tolerance must be greater than zero.");
+            
+            return (double)fitnessResults.Count(f => f <= fitnessResults.Min() + tolerance)
+                   / fitnessResults.Count;
+        }
+
+        public TimeSpan MeanTimeToConverge => TimeSpan.FromMilliseconds(timesToConverge.Average(t => t.Milliseconds));
+        
+        public double MeanEvaluationsToConverge => evaluationsToConverge.Average();
+        
+        public double MeanEvaluationsToFindBest => evaluationsToFindBest.Average();
+
+        public override string ToString()
+        {
+            return $"Testing {optimiser} {fitnessResults.Count} times on {problem}.";
+        }
+    }
+}
---
+++ Optimisation.Problems/HyperparameterOptimisation/OptimiserPerformanceAssessor.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Optimisation.Base.Conversion;
+using Optimisation.Base.Management;
+using Optimisation.Base.Runtime;
+
+namespace Optimisation.Problems.HyperparameterOptimisation
+{
+    public class OptimiserPerformanceAssessor
+    {
+        private readonly OptimiserBuilder builder;
+        private readonly IEvaluator evaluator;
+        private readonly Func<Population, bool> convergenceCheckers;
+        
+        public OptimiserPerformanceAssessor(
+            OptimiserBuilder builder, 
+            IEvaluator evaluator,
+            Func<Population, bool> convergenceCheckers)
+        {
+            this.builder = builder;
+            this.evaluator = evaluator;
+            this.convergenceCheckers = convergenceCheckers;
+        }
+
+        public OptimiserPerformance RunAssessment(int numberOfRestarts, 
+            Action<(int, Population)> reporters, Action<int> iterationReporter,
+            int timeOutEvaluations = 0, TimeSpan? timeOutDuration = null)
+        { 
+            var fitnessValues = new List<double>();
+            var timeToComplete = new List<TimeSpan>();
+            var evaluationsToComplete = new List<int>();
+            var evaluationsToFindBest = new List<int>();
+
+            for (var i = 1; i <= numberOfRestarts; i++)
+            {
+                var timeStart = DateTime.Now;
+                
+                var optimiserRunner = new OptimiserRunnerBasic(builder, evaluator, convergenceCheckers, reporters);
+                optimiserRunner.Run(
+                    reportingFrequency: 1000,
+                    timeOutEvaluations: timeOutEvaluations, 
+                    timeOutDuration: timeOutDuration);
+                
+                fitnessValues.Add(optimiserRunner.BestFound.Value.Fitness);
+                evaluationsToFindBest.Add(optimiserRunner.BestFound.Key);
+                evaluationsToComplete.Add(optimiserRunner.AllEvaluated.Count);
+                timeToComplete.Add(
+                    optimiserRunner.FinalPopulation
+                        .Select(ind => ind.GetProperty<DateTime>(OptimiserPropertyNames.ReinsertionTime))
+                        .OrderByDescending(t => t)
+                        .First()
+                    - timeStart);
+
+                iterationReporter(i);
+            }
+            
+            return new OptimiserPerformance(
+                builder.CreateOptimiser().ToString(),
+                builder.HyperParameters,
+                evaluator.ToString(),
+                fitnessValues, timeToComplete, evaluationsToComplete, evaluationsToFindBest);
+        }
+    }
+}
