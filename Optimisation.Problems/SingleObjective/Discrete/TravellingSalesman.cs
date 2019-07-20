fac6d952245064ab740a41f50327798c16b5ffc3
Clym Stock-Williams <clymsw@gmail.com>
2019-07-20 23:04:31 +0200

    TravellingSalesmanSetup: Parses TSP problem files

--- Optimisation.Problems/SingleObjective/Discrete/TravellingSalesman.cs
+++ Optimisation.Problems/SingleObjective/Discrete/TravellingSalesman.cs
@@ -12,11 +12,15 @@ namespace Optimisation.Problems.SingleObjective.Discrete
             DecisionVector globalOptimum) 
             : base(name, locations, globalOptimum)
         {
+            if (globalOptimum.Vector.Count != locations.Count() + 1)
+                throw new System.ArgumentOutOfRangeException(
+                    nameof(globalOptimum), 
+                    $"The optimum route should be a complete tour of {locations.Count() + 1} stops.");
         }
 
-        public static TravellingSalesman CreateFromFile(string filePath)
+        public static TravellingSalesman CreateFromFile(string problemFilePath)
         {
-            var setup = new TravellingSalesmanSetup(filePath);
+            var setup = new TravellingSalesmanSetup(problemFilePath);
             int numDims = setup.Locations.Count;
             return new TravellingSalesman(
                 setup.LongName,
