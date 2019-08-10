fb44d3363d71dbdede5d21654bc84dcd5c89409b
Clym Stock-Williams <clymsw@gmail.com>
2019-08-10 17:49:52 +0200

    SolutionToFitnessDistanceToPoint, SolutionToFitnessWeightedAverage: added, no tests

---
+++ PopOptBox.Base/Helpers/SolutionToFitnessDistanceToPoint.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PopOptBox.Base.Helpers
+{
+    /// <summary>
+    /// Solution Vector to Fitness conversion for resolving multiple objectives to a single objective
+    /// through calculating the distance to a predefined point.
+    /// <see cref="Management.Optimiser"/>.
+    /// </summary>
+    public class SolutionToFitnessDistanceToPoint
+    {
+        private readonly double[] optimalPoint;
+
+        /// <summary>
+        /// Create a weighting function which converts multiple objectives to one objective.
+        /// </summary>
+        /// <param name="optimalPoint">The location from which the distance should be measured.</param>
+        public SolutionToFitnessDistanceToPoint(IEnumerable<double> optimalPoint)
+        {
+            this.optimalPoint = optimalPoint.ToArray();
+        }
+
+        /// <summary>
+        /// Calculate average distance to the optimal point to return a single objective fitness.
+        /// </summary>
+        /// <param name="solution">The Solution Vector.</param>
+        /// <returns>The Fitness.</returns>
+        /// <exception cref="System.ArgumentOutOfRangeException">Thrown when the optimal point vector and solution vector are different lengths.</exception>
+        public double Weight(double[] solution)
+        {
+            if (solution.Length != optimalPoint.Length)
+                throw new System.ArgumentOutOfRangeException(nameof(solution),
+                    "The number of dimensions does not equal the length of the optimal point vector.");
+
+            return Math.Sqrt(
+                solution.Select((x, i) => Math.Pow(x - optimalPoint[i], 2.0))
+                .Sum());
+        }
+    }
+}
---
+++ PopOptBox.Base/Helpers/SolutionToFitnessWeightedAverage.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PopOptBox.Base.Helpers
+{
+    /// <summary>
+    /// Solution Vector to Fitness conversion for resolving multiple objectives to a single objective
+    /// through applying a weighted average.
+    /// <see cref="Management.Optimiser"/>.
+    /// </summary>
+    public class SolutionToFitnessWeightedAverage
+    {
+        private readonly double[] weights;
+
+        /// <summary>
+        /// Create a weighting function which converts multiple objectives to one objective.
+        /// </summary>
+        /// <param name="weights">
+        /// The weights to multiply each objective by. 
+        /// Dimensions to be maximised should have a negative value.
+        /// </param>
+        public SolutionToFitnessWeightedAverage(IEnumerable<double> weights)
+        {
+            this.weights = weights.ToArray();
+        }
+
+        /// <summary>
+        /// Calculate weighted sum of objectives to return a single objective fitness.
+        /// </summary>
+        /// <param name="solution">The Solution Vector.</param>
+        /// <returns>The Fitness.</returns>
+        /// <exception cref="System.ArgumentOutOfRangeException">Thrown when the weights vector and solution vector are different lengths.</exception>
+        public double Weight(double[] solution)
+        {
+            if (solution.Length != weights.Length)
+                throw new System.ArgumentOutOfRangeException(nameof(solution),
+                    "The number of dimensions does not equal the length of the weight vector.");
+
+            return solution
+                .Select((x, i) => x * weights[i])
+                .Sum();
+        }
+    }
+}
