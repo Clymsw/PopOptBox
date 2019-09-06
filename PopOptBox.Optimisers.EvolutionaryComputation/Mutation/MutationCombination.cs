ae569518133d2ff96cde82b9096f7649939986ea
Clym Stock-Williams <Clym.Stock-Williams@tno.nl>
2019-09-06 13:10:03 +0200

    MutationCombination: added

---
+++ PopOptBox.Optimisers.EvolutionaryComputation/Mutation/MutationCombination.cs
@@ -0,0 +1,12 @@
+using PopOptBox.Base.Variables;
+
+namespace PopOptBox.Optimisers.EvolutionaryComputation.Mutation
+{
+    public class MutationCombination : IMutationOperator
+    {
+        public DecisionVector Operate(DecisionVector decisionVector)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
