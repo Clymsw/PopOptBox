9a1450b3283a13b77b29dc2f4b462eb46df303af
Clym Stock-Williams <clymsw@gmail.com>
2019-10-19 13:38:50 +0200

    MutationAddRandomIntegerFromSet: implemented with tests

---
+++ PopOptBox.Optimisers.EvolutionaryComputation.Test/Mutation/MutationAddRandomIntegerFromSetTests.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Linq;
+using PopOptBox.Base.Variables;
+using Xunit;
+
+namespace PopOptBox.Optimisers.EvolutionaryComputation.Mutation.Test
+{
+    public class MutationAddRandomIntegerFromSetTests
+    {
+        private readonly DecisionVector testDv;
+
+        public MutationAddRandomIntegerFromSetTests()
+        {
+            var discreteDs = DecisionSpace.CreateForUniformIntArray(4, int.MinValue, int.MaxValue);
+            var continuousDs = DecisionSpace.CreateForUniformDoubleArray(4, double.MinValue, double.MaxValue);
+            var variables = discreteDs.ToList();
+            variables.AddRange(continuousDs);
+            testDv = DecisionVector.CreateFromArray(
+                new DecisionSpace(variables),
+                new double[8] { 7, 6, 5, 4, 3.1, 2.1, 1.1, 0.0 });
+        }
+
+        [Fact]
+        public void Construct_InvalidMinMaxValues_Throws()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                new MutationAddRandomIntegerFromSet(
+                    new[] { 1, 3 },
+                    -0.01, 1));
+        }
+
+        [Fact]
+        public void Construct_InvalidMutationProbability_Throws()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                new MutationAddRandomIntegerFromSet(
+                    new[] { 1, 3 },
+                    -0.01, 1));
+
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                new MutationAddRandomIntegerFromSet(
+                    new[] { 1, 3 },
+                    1.01, 1));
+        }
+
+        [Fact]
+        public void Construct_InvalidNumberOfMutations_Throws()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                new MutationAddRandomIntegerFromSet(
+                    new[] { 1, 3 },
+                    0.1, 0));
+        }
+
+        [Fact]
+        public void Operate_OneMutationDesired_ZeroMutationChance_ReturnsOriginalDv()
+        {
+            var mutator = new MutationAddRandomIntegerFromSet(
+                new[] { 1, 3 },
+                0, 1);
+
+            var newDv = mutator.Operate(testDv);
+
+            Assert.Equal(testDv, newDv);
+        }
+
+        [Fact]
+        public void Operate_OneMutationDesired_ReturnsMutatedDv()
+        {
+            var mutator = new MutationAddRandomIntegerFromSet(
+                new[] { 2 },
+                1, 1);
+
+            var newDv = mutator.Operate(testDv);
+
+            Assert.NotEqual(testDv, newDv);
+            Assert.Equal(testDv.Count, newDv.Count);
+            var changedValue = newDv.Where((v, i) => (double)v != (double)testDv.ElementAt(i)).Select(d => (double)d);
+            Assert.True(changedValue.Count() == 1);
+            var oldValue = testDv.Where((v, i) => (double)v != (double)newDv.ElementAt(i)).Select(d => (double)d);
+            Assert.Equal(2, Math.Abs(oldValue.First() - changedValue.First()));
+        }
+
+        [Fact]
+        public void Operate_ManyMutationsDesired_ReturnsMutatedDv()
+        {
+            var mutator = new MutationAddRandomIntegerFromSet(
+                new[] { 2, 7 },
+                1, 10);
+
+            var newDv = mutator.Operate(testDv);
+
+            Assert.NotEqual(testDv, newDv);
+            Assert.Equal(testDv.Count, newDv.Count);
+            Assert.True(newDv.Where((v, i) => v == testDv.ElementAt(i)).Count()
+                        <= testDv.Count - 1);
+        }
+    }
+}
---
+++ PopOptBox.Optimisers.EvolutionaryComputation/Mutation/MutationAddRandomIntegerFromSet.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using PopOptBox.Base.Variables;
+
+namespace PopOptBox.Optimisers.EvolutionaryComputation.Mutation
+{
+    /// <summary>
+    /// A mutation operator for <see cref="VariableContinuous"/> <see cref="DecisionVector"/> elements, which can add a random integer chosen from a set provided.
+    /// </summary>
+    public class MutationAddRandomIntegerFromSet : Operator, IMutationOperator
+    {
+        private readonly RandomNumberManager rngManager;
+        private readonly int[] numberSet;
+        private readonly double mutationProbability;
+        private readonly int maximumNumberOfMutations;
+
+        /// <summary>
+        /// Constructs a mutation operator that adds an integer, chosen randomly from a set of numbers provided, to zero or more elements in the <see cref="DecisionVector"/>.
+        /// </summary>
+        /// <param name="numberSet">The set of numbers to choose from.</param>
+        /// <param name="mutationProbability">The probability that any mutation will occur.</param>
+        /// <param name="maximumNumberOfMutations">The maximum number of times a mutation should be tried.</param>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when any of the input values are illegal.</exception>
+        public MutationAddRandomIntegerFromSet(IEnumerable<int> numberSet, double mutationProbability, int maximumNumberOfMutations)
+            : base($"Add random number from set of size {numberSet.Count()}")
+        {
+            var numbers = numberSet.ToArray();
+
+            if (mutationProbability < 0 || mutationProbability > 1)
+                throw new ArgumentOutOfRangeException(nameof(mutationProbability),
+                    "Mutation probability must be a value between 0 and 1.");
+            this.mutationProbability = mutationProbability;
+
+            if (maximumNumberOfMutations <= 0)
+                throw new ArgumentOutOfRangeException(nameof(maximumNumberOfMutations),
+                    "Maximum number of mutations must be greater than 0.");
+            this.maximumNumberOfMutations = maximumNumberOfMutations;
+
+            this.numberSet = numbers;
+
+            rngManager = new RandomNumberManager();
+        }
+
+        /// <summary>
+        /// Gets a new Decision Vector where elements have potentially been mutated.
+        /// Uses <see cref="IVariable"/> to wrap the added integer, ensuring a valid Decision Vector is always created.
+        /// </summary>
+        /// <param name="decisionVector">The existing Decision Vector.</param>
+        /// <returns>A new Decision Vector.</returns>
+        public DecisionVector Operate(DecisionVector decisionVector)
+        {
+            var locationsToMutate = rngManager.GetLocations(
+                decisionVector.Count, maximumNumberOfMutations,
+                true, mutationProbability);
+
+            var newDv = decisionVector.Select(i => (double)i).ToArray();
+            var newDs = decisionVector.GetDecisionSpace();
+
+            foreach (var location in locationsToMutate)
+            {
+                var mutationLocation = rngManager.Rng.Next(0, numberSet.Length);
+                newDv[location] = Convert.ToDouble(newDs.ElementAt(location).AddOrWrap(newDv[location], numberSet[mutationLocation]));
+            }
+
+            return DecisionVector.CreateFromArray(newDs, newDv);
+        }
+    }
+}
